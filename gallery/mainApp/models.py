e17288f8be569799f9a6ed4eb5dd96c758bd95b0
SpaceScout <nelindanila7@gmail.com>
2024-03-02 15:14:37 +0300

    started making albums logic

--- gallery/mainApp/models.py
+++ gallery/mainApp/models.py
@@ -36,3 +36,13 @@ class CustomUser(AbstractUser):
         verbose_name='user permissions',
         help_text='Specific permissions for this user.',
     )
+
+
+class Album(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='albums')
+    title = models.CharField(max_length=255)
+    allowed_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='allowed_albums', blank=True)
+    photos = models.ManyToManyField('Photo', related_name='albums', blank=True)
+
+    def __str__(self):
+        return f"{self.author.email}'s Album: {self.title}"
